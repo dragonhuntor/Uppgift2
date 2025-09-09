068dd75b920acedf2900b6511b24b4f1d6179aae
dragonhuntor <rafi.nabizada@elev.ga.dbgy.se>
2025-09-09 11:19:12 +0200

    Uppgift 2 Klar

--- analyze_sales.py
+++ analyze_sales.py
@@ -1,44 +1,54 @@
 import csv
 import os
 import locale
+from collections import Counter
 
+#få rätt formaterad valuta
+def format_currency(value):
+    return locale.currency(value,grouping=True)
 
 def load_sales(filename):
-    products = {}  #ordbok dictionary
- 
+    products = {}
+    all_products = []
+
+
     with open(filename, 'r') as file:
         reader = csv.DictReader(file)
-        
         for row in reader:
             product = row['Product']
             sales = float(row['Sales'])
+            
+            all_products.append(product)
             
             if product in products:
                 products[product] += sales
             else:
                 products[product] = sales
                 
-    return products
+    return all_products, products #returnerar products
                 
-def analyze_sales_data(products):
-        
+def analyze_sales_data(all_products, products):    
+    print(products)
+    
     #TODO: Hitta den mest sålda produkten (TIPS! Använd Counter från collections)
-    most_common_product = ("test", 1)
-
+    
+    most_sold = Counter(all_products)
+    most_common_product = most_sold.most_common(1)[0]
+    print(most_sold)
     
     #TODO: Hitta den mest lukrativa produkten med max: max(products, key=products.get)
-    most_lucrative_product = 0
+    most_lucrative_product = max(products, key=products.get)
+    product_value = products[most_lucrative_product]
+        
     
-    print(f"Mest sålda produkt: {most_common_product[0]}, Antal: {most_common_product[1]}")
-    print(f"Mest lukrativa produkt: \"{most_lucrative_product}\" med försäljning på {locale.currency(0,grouping=True)}") #TODO: BONUS: kan du skapa en funktion som skriver ut rätt formaterad valuta istället för detta?
+    print(f"Mest sålda produkt: {most_common_product[0]} Antal: {most_common_product[1]}")  #FIXME: Redovisa mest sålda produkt här
+    print(f"Mest lukrativa produkt: \"{most_lucrative_product}\" med försäljning på {format_currency(product_value)}") #TODO: BONUS: kan du skapa en funktion som skriver ut rätt formaterad valuta istället för detta?
+
 
 
 # Sätt språkinställning till svenska (Sverige) används för att skriva ut formaterad valuta
 locale.setlocale(locale.LC_ALL, 'sv_SE.UTF-8')  
 
 os.system('cls')
-products = load_sales('sales_data.csv')
-
-print(products)
-analyze_sales_data(products)
-
+all_products, products = load_sales('sales_data.csv')
+analyze_sales_data(all_products, products)
